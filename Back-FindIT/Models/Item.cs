dae283a03fe09299d24fcc84a601a3873e8fd4f7
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-31 22:42:36 -0300

    [Feature]: Model Item

---
+++ Back-FindIT/Models/Item.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Back_FindIT.Models
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string? Description { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int RegisteredBy { get; set; }
+
+        [Required]
+        public int ClaimedBy { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; } = null!;
+
+        [ForeignKey("RegisteredBy")]
+        public virtual User RegisteredUser { get; set; } = null!;
+
+        [ForeignKey("ClaimedBy")]
+        public virtual User ClaimedUser { get; set; } = null!;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public void SetUpdatedAt()
+        {
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
